92fb569568c5ebaf5cbf7a040825955eed00ca4c
alex-Linux <vootehdoo@gmail.com>
2024-04-11 16:28:01 +0200

    Style 404page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,11 +2,16 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
-      <h1>Error: 404</h1>
-      <h2>Page not found</h2>
-      <Link to="/">Go home</Link>
-    </>
+    <div className="flex flex-col gap-3 w-fit mx-auto h-fit mt-36">
+      <h1 className="text-6xl text-center">Error: 404</h1>
+      <h2 className="text-center text-xl">Page not found</h2>
+      <Link
+        className="mt-10 text-center w-fit mx-auto py-2 px-4 text-xl rounded-lg bg-emerald-900 hover:bg-emerald-800 border border-emerald-800"
+        to="/"
+      >
+        Go home
+      </Link>
+    </div>
   );
 };
 
