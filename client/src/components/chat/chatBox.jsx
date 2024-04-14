14c7f696ad134e6478b0f77578435b74abf33832
alex-Linux <vootehdoo@gmail.com>
2024-04-14 20:44:18 +0200

    Make messages be displayed on chat component

--- client/src/components/chat/chatBox.jsx
+++ client/src/components/chat/chatBox.jsx
@@ -3,6 +3,7 @@ import { LuMessagesSquare } from "react-icons/lu";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchCurrentChatRecipient } from "../../hooks/useFetchCurrentChatRecipient.js";
+import moment from "moment";
 
 import "ldrs/ring";
 
@@ -12,7 +13,6 @@ const ChatBox = () => {
     useContext(ChatContext);
 
   const { recipientUser } = useFetchCurrentChatRecipient({ currentChat, user });
-
   if (!recipientUser)
     return (
       <>
@@ -45,7 +45,7 @@ const ChatBox = () => {
           </h2>
           <div className="bg-neutral-700 rounded-md border border-neutral-600 h-4/5">
             <div>
-              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg shadow">
+              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg ">
                 <l-ring size={18} color="#00df8e"></l-ring>
               </h3>
             </div>
@@ -54,7 +54,6 @@ const ChatBox = () => {
         </div>
       </>
     );
-
   return (
     <>
       <div className=" w-3/5 h-10/12 h-full">
@@ -66,16 +65,27 @@ const ChatBox = () => {
         <div className="bg-neutral-700 rounded-md border border-neutral-600 h-4/5">
           <div>
             {recipientUser?.name === "alex" ? (
-              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg shadow  text-violet-300 glow-text">
+              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg   text-violet-300 glow-text">
                 {recipientUser?.name}
               </h3>
             ) : (
-              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg shadow">
+              <h3 className="text-center bg-neutral-900 bg-opacity-30 p-2 rounded-tr-lg rounded-tl-lg ">
                 {recipientUser?.name}
               </h3>
             )}
           </div>
-          <div></div>
+
+          {messages?.map((message, index) => (
+            <div
+              key={index}
+              className="bg-neutral-800 bg-opacity-70 w-5/12 px-4 py-2 m-2 rounded-xl"
+            >
+              <h3 className="pb-1">{message.text}</h3>
+              <p className="text-neutral-500 text-end italic text-xs">
+                {moment(message.currentChat).calendar()}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </>
